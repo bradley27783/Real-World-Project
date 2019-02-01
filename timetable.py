9bc9021280b6d475d96bcf3013c8cfcfa49fa229
Joseph McGeever <mcgeevej@uni.coventry.ac.uk>
2019-02-01 13:09:12 +0000

    Stuck on submitting web form using requests

--- timetable.py
+++ timetable.py
@@ -1,9 +1,10 @@
 # import web scraping libraries
 import requests
+
 from bs4 import BeautifulSoup
 
 timetable = "https://webapp.coventry.ac.uk/Timetable-main/Show/7658503?f=&t=&d=20170926&u=EEC/Timetable&k=aB7yw-TFnWGgPYLHzXDz8woGNts.X#view=agendaWeek"
-
+#timetable = "https://webapp.coventry.ac.uk/Timetable-main/Timetable/Current#view=agendaWeek"
     
     
 def scrape(timetable):
@@ -14,8 +15,28 @@ def scrape(timetable):
     
     html = BeautifulSoup(page.content, 'html.parser')
     
-    #print(html.prettify()) #<--dont do that again
+    print(html.prettify()) #<--dont do that again
     
-    print(html.event)
+
+
     
+    
+def getTimetable(url):
+    
+    password = input("Enter password ")
+    data = {
+    'username': 'mcgeevej',
+    'password': password
+     }
+
+
+    page = requests.post(url, data=data)
+    html = BeautifulSoup(page.content, 'html.parser')
+    
+    print(html.prettify())
+    
+
+
+#
 scrape(timetable)
+#getTimetable(timetable)
