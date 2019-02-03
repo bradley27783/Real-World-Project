694795efbda671d0ff5728c0c931f03d641411ee
Joseph McGeever <mcgeevej@uni.coventry.ac.uk>
2019-02-03 15:01:32 +0000

    User is now able to enter their uni login details and the resulting javascript of their timetable is stored to a text file

--- timetable.py
+++ timetable.py
@@ -6,24 +6,17 @@ import requests
 from bs4 import BeautifulSoup
 from requests_ntlm import HttpNtlmAuth
 from getpass import getpass
-timetable = "https://webapp.coventry.ac.uk/Timetable-main/Show/7658503?f=&t=&d=20170926&u=EEC/Timetable&k=aB7yw-TFnWGgPYLHzXDz8woGNts.X#view=agendaWeek"
-#timetable = "https://webapp.coventry.ac.uk/Timetable-main/"
+
+
+timetable = "https://webapp.coventry.ac.uk/Timetable-main/"
     
     
-def scrape(timetable):
+def format(timetable):
     # query website and return the html
     counter = 0
-    s = requests.session()
-    page = requests.get(timetable)
-    
-    print(page.status_code) #200 means a ok :D
-    
-    html = BeautifulSoup(page.content, 'html.parser')
-    
-    html = html.prettify() #<--dont do that again
-    
+    html = BeautifulSoup(timetable.content, 'html.parser')
     f = open("html.txt", "w")
-    f.write(html)
+    f.write(str(html))
     f.close()
     f = open("html.txt", "r")
     lines = f.readlines()
@@ -32,7 +25,7 @@ def scrape(timetable):
     blacklistCounter = 0
     f = open("html.txt", "w")
     for l in lines:
-        if counter >= 202 and l.strip() != '': #and NOT USELESS LINES
+        if counter >= 387 and l.strip() != '':
             if "]" in l:
                 f.write(l)
                 break
@@ -44,6 +37,7 @@ def scrape(timetable):
             f.write(l)
         counter = counter+1
     f.close()
+
     #html.txt now has in key : value pair - the data for each event in the timetable
     #now: https://stackoverflow.com/questions/24027589/how-to-convert-raw-javascript-object-to-python-dictionary/26900181#26900181
     
@@ -57,24 +51,23 @@ def scrape(timetable):
 def getTimetable(url):
     
 
-    pauser = raw_input('Username:           >')
-    pw =   getpass('Password (hidden):  >')
+    user = input('Enter your University login: ')
+    password =   getpass('Now enter your password: ')
 
-    print("Getting timetable source...")
+    print("Retrieving timetable")
 
-    auth = HttpNtlmAuth('\\' + user, pw)
-    r = requests.get("https://webapp.coventry.ac.uk/Timetable-main/Timetable/Current/", auth=auth)
+    auth = HttpNtlmAuth('\\' + user, password)
+    timetable = requests.get("https://webapp.coventry.ac.uk/Timetable-main/Timetable/Current/", auth=auth)
     
-    if r.status_code == 200:
-        print("Successfully loaded timetable:\n" + r.text)
-    elif r.status_code == 401:
-        print("Authentication failed.")
+    if timetable.status_code == 200:
+        print("Completed loading timetable!")
+        format(timetable)
+    elif timetable.status_code == 401:
+        print("Incorrect username/password")
     else:
-        print("Unknown outcome. Response status: " + r.status_code)
+        print("Error! Status code: " + timetable.status_code)
 
 
 
-#
-#
-#scrape(timetable)
 getTimetable(timetable)
+W8 = input()
