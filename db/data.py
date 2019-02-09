c2da19d45e7817b8c2693aae3e32d12470c7bbf1
Aslam Admani (admania) <admania@coventry.ac.uk>
2019-02-09 18:44:32 +0000

    Create data.py

---
+++ db/data.py
@@ -0,0 +1,98 @@
+# database tables go in here
+ecb = { "building":"Engineering and computing building (ECB)",
+
+   "coordinates":"52.4053° N, 1.4997° W",
+
+   "ECG":[  
+      {  
+         "type":"no_PC",
+         "rid":[  
+            "ECG-01",
+            "ECG-02",
+            "ECG-12 (THETA ROOM)",
+            "ECG-13"]
+      },
+      {  
+         "type":"has_PC",
+         "rid":[  
+            "ECG-03",
+            "ECG-04",
+            "ECG-14",
+            "ECG-15",
+            "ECG-36 (ECDL TEST CENTRE)"]
+      },
+      {  
+         "type":"lec_Theatre",
+         "rid":[  
+            "ECG-24 (THE WESTON LECTURE THEATRE)",
+            "ECG-26 (THE WOLFSON LECTURE THEATRE)",
+            "ECG-27 (THE WOLFSON LECTURE THEATRE)"]
+      },
+      {  
+         "type":"lab",
+         "rid":[  
+            "ECG-35 (ELECTRONICS LAB (No food or drinks allowed in lab))"]
+      }
+   ],
+   
+   "EC1":[  
+      {  
+         "type":"no_PC",
+         "rid":[  
+            "EC1-02",
+            "EC1-03",
+            "EC1-11",
+            "EC1-12",
+            "EC1-36 (ECDL TEST CENTRE)",
+            "EC1-21",
+            "EC1-22 (THIS ROOM HAS NO LAPTOPS)",
+            "EC1-23"]
+      },
+      {  
+         "type":"has_PC",
+         "rid":[  
+            "EC1-01 (PROGRAMMING SUPPORT @ WEDNESDAY 1PM-5PM)",
+            "EC1-13 (CISCO LABS)",
+            "EC1-14 ETHICAL HACKING LABS"]
+      },
+      {  
+         "type":"other",
+         "rid":[  
+            "EC1-24 EEC SPARK",
+            "EC1-25 EEC FUTURES (PLACEMENT SUPPORT)",
+            "EC1-27 (PLANET EARTH)",
+            "EC1-28 (PLANET VENUS)",
+            "EC1-29 (THIS ROOM IS AVAILABLE FOR BOOKING. PLEASE CONTACT TIMETABLING.EC)"]
+      }
+   ],
+
+   "EC2":[  
+      {  
+         "type":"no_PC",
+         "rid":[  
+            "EC2-01",
+            "EC2-02",
+            "EC2-03"]
+      },
+      {  
+         "type":"has_PC",
+         "rid":[  
+            "EC2-04 LABS (NO EATING OR DRINKING)",
+            "EC2-12",
+            "EC2-13",
+            "EC2-14"]
+      },
+      {  
+         "type":"other",
+         "rid":[  
+            "EC2-21 FULL TIME PHD RESEARCH STUDENT WORKSPACE",
+            "EC2-24 (PLANET MARS)",
+            "EC2-25 (PLANET MERCURY)"]
+      }
+   ]
+}
+
+library ={
+   "building": "Lanchester libarary",
+   "coordinates": "52.4060° N, 1.5005° W"
+}
